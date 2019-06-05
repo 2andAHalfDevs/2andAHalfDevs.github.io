750f11bfd6f2003f4a7b93bba5c7d1a24eda889a
Joseph Ladino <drtnt25@gmail.com>
2019-06-04 23:13:18 -0400

    landing page css, slight update to fullscreen.js, one more coming soon

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -1,11 +1,18 @@
 // @Two&HalfDevs //
 
 /*
-  Previous update: Unknown
-  Last Updated: June 2, 2019
+  Previous update: June 2, 2019
+  Last Updated: June 4, 2019
 */
 
+/* this'll be useful whenever I finish updating this script
+(function() {
+
+})();
+
 var fullscreen = {
+  using: "", */
+
   enabled: function() {
     if (document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled) {
       return true;
@@ -15,43 +22,65 @@ var fullscreen = {
   },
 
   request: function(element) {
-    this.element = element;
-    if (this.element.requestFullscreen) {
-      this.element.requestFullscreen();
-    } else if (this.element.webkitRequestFullscreen) {
-      this.element.webkitRequestFullscreen();
-    } else if (this.element.mozRequestFullScreen) {
-      this.element.mozRequestFullScreen();
-    } else if (this.element.msRequestFullscreen) {
-      this.element.msRequestFullscreen();
+    if(element) {
+      this.element = element;
+      if(this.addedListener < 1) {
+        document.addEventListener("fullscreenchange", function() {
+          console.log(this.inFullscreen);
+          this.inFullscreen = (document.fullscreenElement) ? false : true;
+          console.log(this.inFullscreen);
+        },false);
+        document.addEventListener("webkitfullscreenchange", function() {
+          this.inFullscreen = (document.fullscreenElement) ? false : true;
+        },false);
+        document.addEventListener("mozfullscreenchange", function() {
+          this.inFullscreen = (!document.fullscreenElement) ? false : true;
+        },false);
+        document.addEventListener("msfullscreenchange", function() {
+          this.inFullscreen = (!document.fullscreenElement) ? false : true;
+        },false);
+        this.addedListener = 1;
+      }
+      if (this.element.requestFullscreen) {
+        this.element.requestFullscreen();
+      } else if (this.element.webkitRequestFullscreen) {
+        this.element.webkitRequestFullscreen();
+        this.using = "webkit";
+      } else if (this.element.mozRequestFullScreen) {
+        this.element.mozRequestFullScreen();
+        this.using = "moz";
+      } else if (this.element.msRequestFullscreen) {
+        this.element.msRequestFullscreen();
+        this.using = "ms";
+      }
+    } else {
+      return "Element not specified.";
     }
   },
 
   exit: function() {
-    if (document.exitFullscreen) {
-	    document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-	    document.mozCancelFullScreen();
-    } else if (document.msExitFullscreen) {
-	    document.msExitFullscreen();
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if(document.webkitExitFullScreen) {
+      document.webkitExitFullScreen();
+    } else if(document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if(document.msExitFullscreen) {
+      document.msExitFullscreen();
     }
   },
 
   active: function() {
-    if(document.fullscreenElement ||
-    document.webkitFullscreenElement ||
-    document.mozFullScreenElement ||
-    document.msFullscreenElement) {
-      return true;
-    } else {
-      return false;
+    let using = this.using;
+    if(using === "") {
+      console.log(document.fullscreenElement? false : true);
+      return document.fullscreenElement? false : true;
+    } else if(using == "webkit") {
+      return document.webkitIsFullScreen? false : true;
+    } else if(using == "moz") {
+      return document.mozFullScreen? false : true;
+    } else if(using == "ms") {
+      return document.msFullscreenElement? false : true;
     }
   }
-    /*
-      case "element":
-
-      break;
-        */
-}
+};
