a14b8580ab39246c01f5062415b77a6418a4cf94
Joseph Ladino <drtnt25@gmail.com>
2019-06-07 19:07:50 -0400

    CONTROLLER SUPPORT IS HERE!

--- games/testGame/main.js
+++ games/testGame/main.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
   var keyEvent = function(event) {
     //event.preventDefault();
-    controller.keyEvent(event.type, event.keyCode);
+    gameController.keyEvent(event.type, event.keyCode);
   };
 
   var resize = function(event) {
@@ -17,29 +17,29 @@ window.onload = function() {
   };
 
   var update = function() {
-    if(controller.left.active) {
+    if(gameController.left.active) {
       game.world.player.moveLeft();
     }
 
-    if(controller.right.active) {
+    if(gameController.right.active) {
       game.world.player.moveRight();
     }
 
-    if(controller.up.active) {
+    if(gameController.up.active) {
       game.world.player.jump();
-      controller.up.active = false;
+      gameController.up.active = false;
     }
 
-    if(controller.down.active) {
+    if(gameController.down.active) {
       game.world.player.duck();
-      controller.down.active = false;
-    } else if(controller.down.keyUp) {
-      game.world.player.duck(controller.down.keyUp);
+      gameController.down.active = false;
+    } else if(gameController.down.keyUp) {
+      game.world.player.duck(gameController.down.keyUp);
     }
     game.update();
   };
 
-  var controller = new Controller();
+  var gameController = new Controller();
   var display = new Display(document.querySelector("canvas"));
   var game = new Game();
   var engine = new Engine(1000/60, render, update);
@@ -47,16 +47,92 @@ window.onload = function() {
   display.buffer.canvas.height = game.world.height;
   display.buffer.canvas.width = game.world.width;
 
-  // gamepad stuff
-
-  var attachGamepad = function() {
-    alert("Gamepad connected");
+  // Gamepad Stuff
+  // @LadinoJoseph on twitter | @Joseph-Ladino on Github
+  
+  
+  var haveEvents = 'ongamepadconnected' in window;
+  var controllers = {};
+  
+  var attachGamepad = function(e) {
+    var gamepad = e.gamepad;
+    controllers[gamepad.index] = gamepad;
+    console.log("Gamepad connected at " + e.gamepad.index);
+    requestAnimationFrame(updateStatus);
   };
 
-  var removeGamepad = function() {
-    alert("Gamepad disconnected")
+  var removeGamepad = function(e) {
+    alert("Gamepad disconnected");
+    delete controllers[gamepad.index];
   };
+  
+  function updateStatus() {
+  if (!haveEvents) {
+    scangamepads();
+  }
 
+    var i = 0;
+    var controller = controllers[0];
+    var vals = [];
+    var axes = [];
+    for (i = 0; i < controller.buttons.length; i++) {
+      var val = controller.buttons[i];
+      var pressed = val == 1.0;
+      if (typeof(val) == "object") {
+        pressed = val.pressed;
+        val = val.value;
+      }
+    var pct = Math.round(val * 100);
+    let choose = pct > 0 ? "keydown" : "keyup";
+    vals[i] = val;
+  }
+  let jumping = vals[0] === 0 ? (vals[12] === 0 ? false : true) : true;
+  let ducking = vals[1] === 0 ? (vals[13] === 0 ? false : true) : true;
+  
+  for (i = 0; i < controller.axes.length; i++) {
+    axes[i] = controller.axes[i].toFixed(2);
+  }
+  
+  if(jumping || axes[1] <= -0.5) {
+    gameController.keyEvent('keydown', 32);
+  } else if(!jumping || axes[1] >= 0.5) {
+    gameController.keyEvent('keyup', 32);
+  }
+  
+  if(ducking || axes[1] >= 0.5) {
+    gameController.keyEvent('keydown', 40);
+  } else if(!ducking || axes[1] <= -0.5) {
+    gameController.keyEvent('keyup', 40);
+  }
+  
+  if(vals[14] == 1 || axes[0] < -0.15) {
+    gameController.keyEvent('keydown', 37);
+  } else {
+    gameController.keyEvent('keyup', 37);
+  }
+  
+  if(vals[15] == 1 || axes[0] > 0.15) {
+    gameController.keyEvent('keydown', 39);
+  } else {
+    gameController.keyEvent('keyup', 39);
+  }
+  
+  requestAnimationFrame(updateStatus);
+}
+  
+  function scangamepads() {
+  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+  for (var i = 0; i < gamepads.length; i++) {
+    if (gamepads[i]) {
+      if (gamepads[i].index in controllers) {
+        controllers[gamepads[i].index] = gamepads[i];
+      } else {
+        attachGamepad(gamepads[i]);
+      }
+    }
+  }
+}
+  
   window.addEventListener("resize", resize);
   window.addEventListener("keydown", keyEvent);
   window.addEventListener("gamepadconnected", attachGamepad);
